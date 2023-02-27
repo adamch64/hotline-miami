4eb1907c5859835664cc9c3d132795b9b6ebfd47
adamch64 <adam.chytra@infotech.edu.pl>
2023-02-27 14:56:32 +0100

    alpha movement

--- hotline miami/Assets/scripts/movement.cs
+++ hotline miami/Assets/scripts/movement.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
+    [Header("keybinds")]
+    [SerializeField] private KeyCode pickupButton = KeyCode.Space;
+    [Header("movement")]
     private Rigidbody2D rb;
     [SerializeField] private float speed;
     float x,y;
+    Vector2 mouseWorldPosition;
+    [Header("weapons")]
+    public Transform currentWeapon;
+    private bool haveWeapon;
+    [SerializeField] private LayerMask weaponMask;
+    [SerializeField] private float range;
+    [SerializeField] private Transform weaponPosition;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +28,11 @@ public class movement : MonoBehaviour
     void Update()
     {
         MyInput();
+        MousePosition();
+        RotatePlayer();
         MovePlayer();
+        PickingUpWeapons();
+        holdWeapon();
     }
 
     void MyInput() 
@@ -26,9 +41,42 @@ public class movement : MonoBehaviour
         y = Input.GetAxisRaw("Vertical");
     }
 
+    void RotatePlayer()
+    {
+        Vector2 direction = (mouseWorldPosition - (Vector2) transform.position).normalized;
+        transform.up = direction;
+    }
+
     void MovePlayer()
     {
-        Vector2 moveVector = new Vector2(x,y).normalized * speed;;
+        Vector2 moveVector = new Vector2(x, y).normalized * speed;
         rb.velocity = moveVector;
+    }
+
+    void MousePosition()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.nearClipPlane;
+        mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+
+    void PickingUpWeapons()
+    {
+        RaycastHit2D pickUp = Physics2D.CircleCast(transform.position, range, Vector2.zero, 0, weaponMask);
+        if(!Input.GetKeyDown(pickupButton))
+            return;
+        if(pickUp)
+        {
+            currentWeapon = pickUp.transform;
+            currentWeapon.parent = weaponPosition;
+            haveWeapon = true;
+        }
+    }
+
+    void holdWeapon()
+    {
+        if(!haveWeapon)
+            return;
+        currentWeapon.position = weaponPosition.position;
     }
 }
